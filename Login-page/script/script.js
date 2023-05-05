c56ea1ae9ef07da4cd95d90efe625004612a6ce1
sunaina47 <73634062+sunaina47@users.noreply.github.com>
2023-05-05 11:38:06 +0530

    Add files via upload

---
+++ Login-page/script/script.js
@@ -0,0 +1,24 @@
+function validateForma(){
+    var formaData = document.getElementById("forma");
+    var name = formaData.elements.namedItem("uname").value;
+    var nameErr = document.getElementById("name-error");
+    name = name.trim();
+
+    if(name == null || name === ""){
+        nameErr = "Name can't be blank";
+        return false;
+    }
+}
+
+function validateFormb(){
+    var formbData = document.getElementById("formb");
+    var psw1 = formbData.elements.namedItem("psw").value;
+    var psw2 = formbData.elements.namedItem("psw-repeat").value;
+
+    if(psw2 == psw1){
+        return true;
+    }
+    else{
+        alert("Password must be same!")
+    }
+}
